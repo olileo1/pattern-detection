c826a12b530c32e9fca86ff38405fa8288ae7b2f
olileo1 <oliver.leodolter@gmx.net>
2018-05-16 14:19:55 +0200

    add pattern fitting fiesta 6 add mid function

--- R/patternFitting.R
+++ R/patternFitting.R
@@ -677,6 +677,7 @@ patternFitting.fiesta4 <- function(y,
     out$m2.df <- n - 3
     out$m2.method <- 'lm'
   })
+  
   return(
     out
   )
@@ -726,6 +727,270 @@ patternFitting.fiesta5 <- function(y,
     out$m1.pat.coef.var <- (out$m1.scale ^ 2) * xp['pattern', 'pattern']
     out$m1.df <- n - 2
     out$m1.method <- 'lm'
+  })
+  
+  return(
+    out
+  )
+}
+
+patternFitting.fiesta6 <- function(y,
+                                   pat = list(
+                                     x = c(0, 0.1, 1),
+                                     y = c(0, 1, 0)
+                                     )
+                                   ) {
+  n <- length(y)
+  y <- y / median(y[1:floor(n / 20)])
+  X <- cbind(
+    intercept = rep(1, n),
+    slope = 1:n,
+    pattern = approx(pat, n = length(y))$y
+  )
+  out <- list()
+  tryCatch({
+    m0 <- lmrob.fit(x = as.matrix(X[, c('intercept', 'slope')]),
+                    y = y,
+                    control = lmrob.control(setting = 'KS2011',
+                                            maxit.scale = 10000,
+                                            max.it = 10000,
+                                            k.max = 10000,
+                                            refine.tol = 1e-6,
+                                            rel.tol = 1e-6,
+                                            solve.tol = 1e-6))
+    out$m0.scale <- m0$scale
+    fit <- m0$fitted.values
+    res <- m0$residuals
+    res.s <- movingMedian(res, n.back = 3, n.ahead = 3)
+    out$m0.sd <- sd(res)
+    out$m0.sd.end <- sd(mid(res, alpha.begin = 0.2, alpha.end = 0))
+    out$m0.sd.begin <- sd(mid(res, alpha.begin = 0, alpha.end = 0.2))
+    out$m0.sd.mid <- sd(mid(res, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m0.lrvar <- lrvar(res) * n
+    out$m0.lrvar.end <- lrvar(mid(res, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m0.lrvar.begin <- lrvar(mid(res, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m0.lrvar.mid <- lrvar(mid(res, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m0.lrvar.norm <- out$m0.lrvar / (out$m0.sd ^ 2)
+    out$m0.lrvar.norm.end <- out$m0.lrvar.end / (out$m0.sd.end ^ 2)
+    out$m0.lrvar.norm.begin <- out$m0.lrvar.begin / (out$m0.sd.begin ^ 2)
+    out$m0.lrvar.norm.mid <- out$m0.lrvar.mid / (out$m0.sd.mid ^ 2)
+    out$m0.sd.s <- sd(res.s)
+    out$m0.sd.s.end <- sd(mid(res.s, alpha.begin = 0.2, alpha.end = 0))
+    out$m0.sd.s.begin <- sd(mid(res.s, alpha.begin = 0, alpha.end = 0.2))
+    out$m0.sd.s.mid <- sd(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m0.lrvar.s <- lrvar(res.s) * n
+    out$m0.lrvar.s.end <- lrvar(mid(res.s, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m0.lrvar.s.begin <- lrvar(mid(res.s, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m0.lrvar.s.mid <- lrvar(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m0.lrvar.s.norm <- out$m0.lrvar.s / (out$m0.sd.s ^ 2)
+    out$m0.lrvar.s.norm.end <- out$m0.lrvar.s.end / (out$m0.sd.s.end ^ 2)
+    out$m0.lrvar.s.norm.begin <- out$m0.lrvar.s.begin / (out$m0.sd.s.begin ^ 2)
+    out$m0.lrvar.s.norm.mid <- out$m0.lrvar.s.mid / (out$m0.sd.s.mid ^ 2)
+    out$m0.slope.coef <- m0$coefficients['slope']
+    out$m0.slope.coef.var <- m0$cov['slope', 'slope']
+    out$m0.df <- m0$df.residual
+    out$m0.method <- 'lmrob'
+  },
+  warning = function(w) {
+    x <- as.matrix(X[, c('intercept', 'slope')])
+    xp <- solve(t(x) %*% x)
+    coef <- (xp %*% t(x) %*% y)[,1]
+    res <- (y - x %*% coef)[,1]
+    res.s <- movingMedian(res, n.back = 3, n.ahead = 3)
+    fit <- (x %*% coef)[,1]
+    out$m0.scale <- sqrt(sum(residuals ^ 2) / (n - 2))
+    out$m0.sd <- sd(res)
+    out$m0.sd.end <- sd(mid(res, alpha.begin = 0.2, alpha.end = 0))
+    out$m0.sd.begin <- sd(mid(res, alpha.begin = 0, alpha.end = 0.2))
+    out$m0.sd.mid <- sd(mid(res, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m0.lrvar <- lrvar(res) * n
+    out$m0.lrvar.end <- lrvar(mid(res, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m0.lrvar.begin <- lrvar(mid(res, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m0.lrvar.mid <- lrvar(mid(res, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m0.lrvar.norm <- out$m0.lrvar / (out$m0.sd ^ 2)
+    out$m0.lrvar.norm.end <- out$m0.lrvar.end / (out$m0.sd.end ^ 2)
+    out$m0.lrvar.norm.begin <- out$m0.lrvar.begin / (out$m0.sd.begin ^ 2)
+    out$m0.lrvar.norm.mid <- out$m0.lrvar.mid / (out$m0.sd.mid ^ 2)
+    out$m0.sd.s <- sd(res.s)
+    out$m0.sd.s.end <- sd(mid(res.s, alpha.begin = 0.2, alpha.end = 0))
+    out$m0.sd.s.begin <- sd(mid(res.s, alpha.begin = 0, alpha.end = 0.2))
+    out$m0.sd.s.mid <- sd(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m0.lrvar.s <- lrvar(res.s) * n
+    out$m0.lrvar.s.end <- lrvar(mid(res.s, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m0.lrvar.s.begin <- lrvar(mid(res.s, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m0.lrvar.s.mid <- lrvar(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m0.lrvar.s.norm <- out$m0.lrvar.s / (out$m0.sd.s ^ 2)
+    out$m0.lrvar.s.norm.end <- out$m0.lrvar.s.end / (out$m0.sd.s.end ^ 2)
+    out$m0.lrvar.s.norm.begin <- out$m0.lrvar.s.begin / (out$m0.sd.s.begin ^ 2)
+    out$m0.lrvar.s.norm.mid <- out$m0.lrvar.s.mid / (out$m0.sd.s.mid ^ 2)
+    out$m0.slope.coef <- coef['slope']
+    out$m0.slope.coef.var <- (out$m0.scale ^ 2) * xp['slope', 'slope']
+    out$m0.df <- n - 2
+    out$m0.method <- 'lm'
+  })
+  tryCatch({
+    m1 <- lmrob.fit(x = as.matrix(X[, c('intercept', 'pattern')]),
+                    y = y,
+                    control = lmrob.control(setting = 'KS2011',
+                                            maxit.scale = 10000,
+                                            max.it = 10000,
+                                            k.max = 10000,
+                                            refine.tol = 1e-6,
+                                            rel.tol = 1e-6,
+                                            solve.tol = 1e-6))
+    out$m1.scale <- m1$scale
+    fit <- m1$fitted.values
+    res <- m1$residuals
+    res.s <- movingMedian(res, n.back = 3, n.ahead = 3)
+    out$m1.sd <- sd(res)
+    out$m1.sd.end <- sd(mid(res, alpha.begin = 0.2, alpha.end = 0))
+    out$m1.sd.begin <- sd(mid(res, alpha.begin = 0, alpha.end = 0.2))
+    out$m1.sd.mid <- sd(mid(res, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m1.lrvar <- lrvar(res) * n
+    out$m1.lrvar.end <- lrvar(mid(res, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m1.lrvar.begin <- lrvar(mid(res, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m1.lrvar.mid <- lrvar(mid(res, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m1.lrvar.norm <- out$m1.lrvar / (out$m1.sd ^ 2)
+    out$m1.lrvar.norm.end <- out$m1.lrvar.end / (out$m1.sd.end ^ 2)
+    out$m1.lrvar.norm.begin <- out$m1.lrvar.begin / (out$m1.sd.begin ^ 2)
+    out$m1.lrvar.norm.mid <- out$m1.lrvar.mid / (out$m1.sd.mid ^ 2)
+    out$m1.sd.s <- sd(res.s)
+    out$m1.sd.s.end <- sd(mid(res.s, alpha.begin = 0.2, alpha.end = 0))
+    out$m1.sd.s.begin <- sd(mid(res.s, alpha.begin = 0, alpha.end = 0.2))
+    out$m1.sd.s.mid <- sd(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m1.lrvar.s <- lrvar(res.s) * n
+    out$m1.lrvar.s.end <- lrvar(mid(res.s, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m1.lrvar.s.begin <- lrvar(mid(res.s, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m1.lrvar.s.mid <- lrvar(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m1.lrvar.s.norm <- out$m1.lrvar.s / (out$m1.sd.s ^ 2)
+    out$m1.lrvar.s.norm.end <- out$m1.lrvar.s.end / (out$m1.sd.s.end ^ 2)
+    out$m1.lrvar.s.norm.begin <- out$m1.lrvar.s.begin / (out$m1.sd.s.begin ^ 2)
+    out$m1.lrvar.s.norm.mid <- out$m1.lrvar.s.mid / (out$m1.sd.s.mid ^ 2)
+    out$m1.pat.coef <- m1$coefficients['pattern']
+    out$m1.pat.coef.var <- m1$cov['pattern', 'pattern']
+    out$m1.df <- m1$df.residual
+    out$m1.method <- 'lmrob'
+  },
+  warning = function(w) {
+    x <- as.matrix(X[, c('intercept', 'pattern')])
+    xp <- solve(t(x) %*% x)
+    coef <- (xp %*% t(x) %*% y)[,1]
+    res <- (y - x %*% coef)[,1]
+    res.s <- movingMedian(res, n.back = 3, n.ahead = 3)
+    fit <- (x %*% coef)[,1]
+    out$m1.scale <- sqrt(sum(residuals ^ 2) / (n - 2))
+    out$m1.sd <- sd(res)
+    out$m1.sd.end <- sd(mid(res, alpha.begin = 0.2, alpha.end = 0))
+    out$m1.sd.begin <- sd(mid(res, alpha.begin = 0, alpha.end = 0.2))
+    out$m1.sd.mid <- sd(mid(res, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m1.lrvar <- lrvar(res) * n
+    out$m1.lrvar.end <- lrvar(mid(res, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m1.lrvar.begin <- lrvar(mid(res, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m1.lrvar.mid <- lrvar(mid(res, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m1.lrvar.norm <- out$m1.lrvar / (out$m1.sd ^ 2)
+    out$m1.lrvar.norm.end <- out$m1.lrvar.end / (out$m1.sd.end ^ 2)
+    out$m1.lrvar.norm.begin <- out$m1.lrvar.begin / (out$m1.sd.begin ^ 2)
+    out$m1.lrvar.norm.mid <- out$m1.lrvar.mid / (out$m1.sd.mid ^ 2)
+    out$m1.sd.s <- sd(res.s)
+    out$m1.sd.s.end <- sd(mid(res.s, alpha.begin = 0.2, alpha.end = 0))
+    out$m1.sd.s.begin <- sd(mid(res.s, alpha.begin = 0, alpha.end = 0.2))
+    out$m1.sd.s.mid <- sd(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m1.lrvar.s <- lrvar(res.s) * n
+    out$m1.lrvar.s.end <- lrvar(mid(res.s, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m1.lrvar.s.begin <- lrvar(mid(res.s, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m1.lrvar.s.mid <- lrvar(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m1.lrvar.s.norm <- out$m1.lrvar.s / (out$m1.sd.s ^ 2)
+    out$m1.lrvar.s.norm.end <- out$m1.lrvar.s.end / (out$m1.sd.s.end ^ 2)
+    out$m1.lrvar.s.norm.begin <- out$m1.lrvar.s.begin / (out$m1.sd.s.begin ^ 2)
+    out$m1.lrvar.s.norm.mid <- out$m1.lrvar.s.mid / (out$m1.sd.s.mid ^ 2)
+    out$m1.pat.coef <- coef['pattern']
+    out$m1.pat.coef.var <- (out$m1.scale ^ 2) * xp['pattern', 'pattern']
+    out$m1.df <- n - 2
+    out$m1.method <- 'lm'
+  })
+  tryCatch({
+    m2 <- lmrob.fit(x = as.matrix(X[, c('intercept', 'slope', 'pattern')]),
+                    y = y,
+                    control = lmrob.control(setting = 'KS2011',
+                                            maxit.scale = 10000,
+                                            max.it = 10000,
+                                            k.max = 10000,
+                                            refine.tol = 1e-6,
+                                            rel.tol = 1e-6,
+                                            solve.tol = 1e-6))
+    out$m2.scale <- m2$scale
+    fit <- m2$fitted.values
+    res <- m2$residuals
+    res.s <- movingMedian(res, n.back = 3, n.ahead = 3)
+    out$m2.sd <- sd(res)
+    out$m2.sd.end <- sd(mid(res, alpha.begin = 0.2, alpha.end = 0))
+    out$m2.sd.begin <- sd(mid(res, alpha.begin = 0, alpha.end = 0.2))
+    out$m2.sd.mid <- sd(mid(res, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m2.lrvar <- lrvar(res) * n
+    out$m2.lrvar.end <- lrvar(mid(res, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m2.lrvar.begin <- lrvar(mid(res, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m2.lrvar.mid <- lrvar(mid(res, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m2.lrvar.norm <- out$m2.lrvar / (out$m2.sd ^ 2)
+    out$m2.lrvar.norm.end <- out$m2.lrvar.end / (out$m2.sd.end ^ 2)
+    out$m2.lrvar.norm.begin <- out$m2.lrvar.begin / (out$m2.sd.begin ^ 2)
+    out$m2.lrvar.norm.mid <- out$m2.lrvar.mid / (out$m2.sd.mid ^ 2)
+    out$m2.sd.s <- sd(res.s)
+    out$m2.sd.s.end <- sd(mid(res.s, alpha.begin = 0.2, alpha.end = 0))
+    out$m2.sd.s.begin <- sd(mid(res.s, alpha.begin = 0, alpha.end = 0.2))
+    out$m2.sd.s.mid <- sd(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m2.lrvar.s <- lrvar(res.s) * n
+    out$m2.lrvar.s.end <- lrvar(mid(res.s, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m2.lrvar.s.begin <- lrvar(mid(res.s, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m2.lrvar.s.mid <- lrvar(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m2.lrvar.s.norm <- out$m2.lrvar.s / (out$m2.sd.s ^ 2)
+    out$m2.lrvar.s.norm.end <- out$m2.lrvar.s.end / (out$m2.sd.s.end ^ 2)
+    out$m2.lrvar.s.norm.begin <- out$m2.lrvar.s.begin / (out$m2.sd.s.begin ^ 2)
+    out$m2.lrvar.s.norm.mid <- out$m2.lrvar.s.mid / (out$m2.sd.s.mid ^ 2)
+    out$m2.pat.coef <- m2$coefficients['pattern']
+    out$m2.pat.coef.var <- m2$cov['pattern', 'pattern']
+    out$m2.slope.coef <- m2$coefficients['slope']
+    out$m2.slope.coef.var <- m2$cov['slope', 'slope']
+    out$m2.df <- m2$df.residual
+    out$m2.method <- 'lmrob'
+  },
+  warning = function(w) {
+    x <- as.matrix(X[, c('intercept', 'slope', 'pattern')])
+    xp <- solve(t(x) %*% x)
+    coef <- (xp %*% t(x) %*% y)[,1]
+    res <- (y - x %*% coef)[,1]
+    res.s <- movingMedian(res, n.back = 3, n.ahead = 3)
+    fit <- (x %*% coef)[,1]
+    out$m2.scale <- sqrt(sum(residuals ^ 2) / (n - 2))
+    out$m2.sd <- sd(res)
+    out$m2.sd.end <- sd(mid(res, alpha.begin = 0.2, alpha.end = 0))
+    out$m2.sd.begin <- sd(mid(res, alpha.begin = 0, alpha.end = 0.2))
+    out$m2.sd.mid <- sd(mid(res, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m2.lrvar <- lrvar(res) * n
+    out$m2.lrvar.end <- lrvar(mid(res, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m2.lrvar.begin <- lrvar(mid(res, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m2.lrvar.mid <- lrvar(mid(res, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m2.lrvar.norm <- out$m2.lrvar / (out$m2.sd ^ 2)
+    out$m2.lrvar.norm.end <- out$m2.lrvar.end / (out$m2.sd.end ^ 2)
+    out$m2.lrvar.norm.begin <- out$m2.lrvar.begin / (out$m2.sd.begin ^ 2)
+    out$m2.lrvar.norm.mid <- out$m2.lrvar.mid / (out$m2.sd.mid ^ 2)
+    out$m2.sd.s <- sd(res.s)
+    out$m2.sd.s.end <- sd(mid(res.s, alpha.begin = 0.2, alpha.end = 0))
+    out$m2.sd.s.begin <- sd(mid(res.s, alpha.begin = 0, alpha.end = 0.2))
+    out$m2.sd.s.mid <- sd(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1))
+    out$m2.lrvar.s <- lrvar(res.s) * n
+    out$m2.lrvar.s.end <- lrvar(mid(res.s, alpha.begin = 0.2, alpha.end = 0)) * n * 0.8
+    out$m2.lrvar.s.begin <- lrvar(mid(res.s, alpha.begin = 0, alpha.end = 0.2)) * n * 0.8
+    out$m2.lrvar.s.mid <- lrvar(mid(res.s, alpha.begin = 0.1, alpha.end = 0.1)) * n * 0.8
+    out$m2.lrvar.s.norm <- out$m2.lrvar.s / (out$m2.sd.s ^ 2)
+    out$m2.lrvar.s.norm.end <- out$m2.lrvar.s.end / (out$m2.sd.s.end ^ 2)
+    out$m2.lrvar.s.norm.begin <- out$m2.lrvar.s.begin / (out$m2.sd.s.begin ^ 2)
+    out$m2.lrvar.s.norm.mid <- out$m2.lrvar.s.mid / (out$m2.sd.s.mid ^ 2)
+    out$m2.pat.coef <- coef['pattern']
+    out$m2.pat.coef.var <- (out$m2.scale ^ 2) * xp['pattern', 'pattern']
+    out$m2.slope.coef <- coef['slope']
+    out$m2.slope.coef.var <- (out$m2.scale ^ 2) * xp['slope', 'slope']
+    out$m2.df <- n - 3
+    out$m2.method <- 'lm'
   })
   return(
     out
--- R/robustScaling.R
+++ R/robustScaling.R
@@ -17,3 +17,10 @@ robustScalingMAD <- function(x) {
   mad <- median(abs(x - med))
   return((x - med) / mad)
 }
+
+mid <- function(x, alpha.begin = 0.1, alpha.end = 0.1) {
+  n <- length(x)
+  start <- ceiling(alpha.begin * n)
+  end <- floor((1 - alpha.end) * n)
+  return(x[max(1, start) : min(n, end)])
+}
